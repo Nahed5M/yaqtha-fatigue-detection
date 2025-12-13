320ec61d8e04f87a7b039ed83ddbc77b5653d56c
Nahed5M <140528415+Nahed5M@users.noreply.github.com>
2025-12-13 07:01:44 +0300

    Update LED pin assignments and simplify logic

--- fatigue_gate.ino
+++ fatigue_gate.ino
@@ -1,43 +1,39 @@
-//تكامل نظام كشف الإرهاق مع بوابة المصنع (نموذج مبدئي)
-// Arduino جزء الإشارات البصرية والصوتية
 
-int ledGreen = 8;   // LED أخضر - سائق سليم
-int ledRed   = 9;   // LED أحمر - سائق مرهق
-int buzzer   = 10;  // جرس إنذار
+
+int ledGreen = 9;   
+int ledRed   = 5;   
+int buzzer   = 10;  
 
 void setup() {
   pinMode(ledGreen, OUTPUT);
   pinMode(ledRed, OUTPUT);
   pinMode(buzzer, OUTPUT);
 
-  //تشغيل السيريال لاستقبال الأوامر من البايثون
   Serial.begin(9600);
 
-  // افتراضياً: سائق سليم
+
   digitalWrite(ledGreen, HIGH);
   digitalWrite(ledRed, LOW);
   digitalWrite(buzzer, LOW);
 }
 
 void loop() {
-  //إذا وصل أمر من البايثون
   if (Serial.available() > 0) {
     char cmd = Serial.read();
 
     if (cmd == 'G') {
-      // G = Good / Green (سائق سليم)
+
       digitalWrite(ledGreen, HIGH);
       digitalWrite(ledRed, LOW);
-      digitalWrite(buzzer, LOW);
+      noTone(buzzer);
     }
 
-    if (cmd == 'R') {
-      // R = Risk / Red (سائق مرهق)
+    else if (cmd == 'R') {
+
       digitalWrite(ledGreen, LOW);
       digitalWrite(ledRed, HIGH);
 
-      //نبضة إنذار صوتي بسيطة
-      tone(buzzer, 1000);   // 1000 Hz
+      tone(buzzer, 1000);
       delay(300);
       noTone(buzzer);
     }
